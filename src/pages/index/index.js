dd55533401bc76387b538452fcadab86e2f8dc34
Nata Malinova <rawr@raery.ru>
2022-08-31 04:44:09 +0700

    main page

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,1 +1,9 @@
 console.log('main page scripts loaded')
+
+// $(document).ready(function () {
+
+//     $(document).on("click", ".fast-links__title", function () {
+//         $(this).toggleClass("open");
+//     });
+
+// });
